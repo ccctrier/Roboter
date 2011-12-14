c10d9b3981141abf6c788b3b9131b95f4311ec4d
Juice <juice@c3t.de>
2011-12-14 22:59:17 +0100

    add system.exit on app end

--- src/CCCT.java
+++ src/CCCT.java
@@ -1,4 +1,4 @@
-import lejos.nxt.Button;
+import lejos.nxt.LCD;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
@@ -7,22 +7,35 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class CCCT {
 	DifferentialPilot pilot;
 	TouchSensor bump = new TouchSensor(SensorPort.S1);
+	static boolean appRunning = true;
 
 	public void go() {
-		pilot.travel(20, true);
+		pilot.travel(80, true);
 		while (pilot.isMoving()) {
 			if (bump.isPressed())
 				pilot.stop();
 		}
-		System.out.println(" " + pilot.getMovement().getDistanceTraveled());
-		Button.waitForPress();
+		// System.out.println(" " + pilot.getMovement().getDistanceTraveled());
+		appRunning = false;
+
+		// Button.waitForPress();
+		System.exit(0);
 	}
 
 	public static void main(String[] args) {
-		Motor.C.rotate(180);
-		Motor.C.rotate(-180);
-		Motor.C.rotate(180);
-		Motor.C.rotate(-180);
+		LCD.drawString("CCC Trier", 0, 0);
+		Runnable r = new Runnable() {
+			public void run() {
+				while (appRunning) {
+					Motor.C.rotate(80);
+					Motor.C.rotate(-80);
+					Motor.C.rotate(80);
+					Motor.C.rotate(-80);
+				}
+			}
+		};
+		Thread t = new Thread(r);
+		t.start();
 
 		// File file = new File("NyanCatJazz.wav");
 		// Sound.playSample(file, 100);
