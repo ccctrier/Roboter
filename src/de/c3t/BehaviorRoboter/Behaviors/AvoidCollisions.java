1d07acf07f09508d304dbc65790819b7c89d1e9b
Kevin Kirsch <keine.ahnung.ka@googlemail.com>
2011-12-28 19:52:09 +0100

    AvoidCollisions: now turnes the robot by 20° (previous: 10°)

--- src/de/c3t/BehaviorRoboter/Behaviors/AvoidCollisions.java
+++ src/de/c3t/BehaviorRoboter/Behaviors/AvoidCollisions.java
@@ -32,7 +32,7 @@ public class AvoidCollisions implements Behavior {
 	@Override
 	public void action() {
 		Main.pilot.travel(-20);
-		Main.pilot.rotate(10);
+		Main.pilot.rotate(20);
 	}
 
 	@Override
